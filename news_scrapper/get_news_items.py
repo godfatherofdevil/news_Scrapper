32a63829f1be8276f92de28abd6f402c165f0ab1
Avinash Kumar <avi410vikram@gmail.com>
2019-07-02 21:30:15 +0300

    Added scrapping functionality for more sections of BBC site

--- news_scrapper/get_news_items.py
+++ news_scrapper/get_news_items.py
@@ -2,32 +2,66 @@ import requests
 from bs4 import BeautifulSoup
 
 BBC_BASE_URL = "https://www.bbc.com"
+ALL_BBC_BASE_URL = set(
+    ["https://www.bbc.com", "http://www.bbc.com", "https://www.bbc.co.uk", "http://www.bbc.co.uk"]
+    )
+chapter_class_1 = ["news", "sport", "weather"]
+chapter_class_2 = ["travel", "culture", "capital", "future"]
+chapter_class_3 = ["food"]
+chapter_class_4 = ["arts"]
+chapter_class_5 = ["bitesize"]
 
 def get_items(chapter="news", n_items=10):
     """
     This function takes in a name of chapter (like "news" or "sports")
-    and number of items required from that chapter and returns it
+    and number of items required from that chapter and returns a list
+    of tuples having title and url
     """
     n_items = int(n_items)
     request_url = BBC_BASE_URL + "/" + chapter
     exception_message = None
+    title_and_url = []
+
     try:
-        data = requests.get(request_url)
+        with requests.Session() as s:
+            data = s.get(request_url)
     except requests.exceptions.RequestException as e:
         exception_message = "An fatal exception happened while getting news : {}, try again".format(e)
     if exception_message is not None:
         return exception_message
 
-    title_and_url = []
-
     soup = BeautifulSoup(data.content, "html.parser")
+    all_headings = []
 
-    all_headings = soup.find_all('a', class_="gs-c-promo-heading")
+    if chapter in chapter_class_1:
+        all_headings = soup.find_all('a', class_="gs-c-promo-heading")
+    elif chapter in chapter_class_2:
+        all_headings = soup.find_all('div', class_ = "promo-unit-header")
+    elif chapter in chapter_class_3:
+        all_headings = soup.find_all('h3', class_ = "promo__title")
+    elif chapter in chapter_class_4:
+        all_headings = soup.find_all('div', class_ = "k-promo-title")
+    elif chapter in chapter_class_5:
+        all_headings = soup.find_all('h2', class_ = "promo-title")
 
     for heading in all_headings[:n_items]:
         try:
-            title_and_url.append((heading.h3.text, BBC_BASE_URL + heading['href']))
+            if chapter in chapter_class_1:
+                title_and_url.append((heading.h3.text, make_url(heading['href'])))
+            elif chapter in chapter_class_2:
+                title_and_url.append((heading.h3.text, make_url(heading.select('a')[1]['href'])))
+            elif chapter in set(chapter_class_3 + chapter_class_4 + chapter_class_5):
+                title_and_url.append((heading.text, make_url(heading.find_parent('a')['href'])))
         except Exception:
             continue
     
     return title_and_url
+
+def make_url(url):
+    """
+    utility function to make full url from relative url
+    """
+    for base_url in ALL_BBC_BASE_URL:
+        if base_url in url:
+            return url
+    return BBC_BASE_URL + url

7eec75fd987dc8af96a9b784aa63b8efd4572ffe
Avinash Kumar <avi410vikram@gmail.com>
2019-07-03 13:11:29 +0300

    modify DEBUG to False

--- news_service/settings.py
+++ news_service/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')mp54!y&ir53%725p5hu)w3s$x-7kmi6pw5j&0@s#ch+9wo(j#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['radiant-depths-22682.herokuapp.com']
 
